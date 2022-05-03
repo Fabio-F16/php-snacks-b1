ddfea9f532f1f3c1d874d65593520aa7b36b7996
Fabio-F16 <fipsfabio95@gmail.com>
2022-05-03 18:01:12 +0200

    Snack 4

--- index.php
+++ index.php
@@ -97,3 +97,19 @@ while (count($numbers) <  15){
             }
         }
 ?>
+
+<!-- Snack 4
+Prendere un paragrafo abbastanza lungo, contenente diverse frasi. 
+Prendere il paragrafo e suddividerlo in tanti paragrafi. Ogni punto un nuovo paragrafo. -->
+<h2>Snack 4</h2>
+<?php
+$paragraph = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+
+$moreParagraph = explode("." , $paragraph);
+// var_dump($moreParagraph);
+for ($i = 0; $i < count($moreParagraph); $i++){
+    echo('<p>' . $moreParagraph[$i] . '</p>');
+}
+
+
+?>
